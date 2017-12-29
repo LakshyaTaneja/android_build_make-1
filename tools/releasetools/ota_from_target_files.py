07fae3ec136ecf5e23620d7fdcc6b2474a8a22ae
LakshyaTaneja <taneja.lakshay5@gmail.com>
2017-12-29 11:37:22 +0000

    build: Elixer print during recovery install

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -508,6 +508,19 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   if "selinux_fc" in OPTIONS.info_dict:
     WritePolicyConfig(OPTIONS.info_dict["selinux_fc"], output_zip)
 
+    model = GetBuildProp("ro.product.model", OPTIONS.info_dict)
+    build = GetBuildProp("ro.build.date", OPTIONS.info_dict)
+
+    script.Print("-------- ELIXIR OS -------");
+    script.Print("**** *    ***** *   * ***** **** ");
+    script.Print("*    *      *    * *    *   * * ");
+    script.Print("***  *      *     *     *   *  ");
+    script.Print("*    *      *    * *    *   * * ");
+    script.Print("**** **** ***** *   * ***** *  * ");
+    script.Print("- By LakshyaTaneja---- 8.1.0_r2 -");
+    script.Print(" Compiled: %s "%(build));
+    script.Print(" For: %s   "%(model));
+    script.Print("----------------------------------------------");
   recovery_mount_options = OPTIONS.info_dict.get("recovery_mount_options")
 
   script.ShowProgress(system_progress, 0)
